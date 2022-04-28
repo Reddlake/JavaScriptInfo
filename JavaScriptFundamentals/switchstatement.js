33036e42081c1353bb36a2968cd85999d7114312
Reddlake <reddlake4eva@gmail.com>
2022-04-28 16:05:22 -0300

    Did the second task

--- JavaScriptFundamentals/switchstatement.js
+++ JavaScriptFundamentals/switchstatement.js
@@ -2,7 +2,7 @@
 
 // Write the code using if..else which would correspond to the following switch:
 
-switch (browser) {
+/* switch (browser) {
     case 'Edge':
       alert( "You've got the Edge!" );
       break;
@@ -24,4 +24,34 @@ switch (browser) {
       alert('Okay we support these browsers too');
   } else {
       alert('We hope that this page looks ok!');
-  }
+  } */
+
+
+  // 2: Rewrite the code below using a single switch statement:
+
+  let a = +prompt('a?', '');
+
+/* if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+} */
+
+switch(a){
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2,3');
+        break;
+
+}
